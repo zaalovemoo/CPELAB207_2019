a3ba2d135137ca62175aee36dd7739af6141d76b
zaalovemoo <46591200+zaalovemoo@users.noreply.github.com>
2019-12-27 14:45:24 +0700

    Add files via upload

--- js/ahitainah.js
+++ js/ahitainah.js
@@ -1,1 +1,8 @@
-
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+    iframe.style.margin= "auto";
+  }
